b69ee88d4f04e1a7ff6b90677f2dfad4ba65c140
Hongjia Liu <hongjia_liu@outlook.com>
2022-03-08 21:45:08 +0000

    Update 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,30 +1,21 @@
 import Link from "next/link";
-
+import Container from "../components/Container";
 export default function Custom404() {
 	return (
-		<div className="flex min-h-full flex-col justify-center bg-white pt-16 pb-12">
-			<main className="mx-auto flex w-full max-w-7xl flex-grow flex-col justify-center px-4 sm:px-6 lg:px-8">
-				<div className="py-16">
-					<div className="text-center">
-						<p className="text-md font-semibold uppercase tracking-wide text-cyan-600">
-							404 error
-						</p>
-						<h1 className="mt-2 text-4xl font-extrabold tracking-tight text-gray-900 sm:text-6xl">
-							PAGE NOT FOUND.
-						</h1>
-						<p className="mt-2 text-xl text-gray-500">
-							Sorry, we couldn’t find the page you’re looking for.
-						</p>
-						<div className="mt-6">
-							<Link href="/">
-								<a className="text-lg font-medium text-cyan-600 hover:text-cyan-500">
-									Go back home →
-								</a>
-							</Link>
-						</div>
-					</div>
-				</div>
-			</main>
-		</div>
+		<Container>
+			<div className="mx-auto mt-32 mb-16 flex max-w-2xl flex-col items-start justify-center">
+				<h1 className="mb-4 text-3xl font-bold tracking-tight text-black dark:text-white md:text-5xl">
+					404 - PAGE NOT FOUND
+				</h1>
+				<p className="mx-auto mb-8 text-center text-gray-600 dark:text-gray-400">
+					Sorry, we couldn’t find the page you’re looking for.
+				</p>
+				<Link href="/">
+					<a className="mx-auto w-64 rounded-md bg-gray-200 p-1 text-center font-bold text-black dark:bg-gray-800 dark:text-white sm:p-4">
+						Return Home
+					</a>
+				</Link>
+			</div>
+		</Container>
 	);
 }
